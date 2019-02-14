d1b86372c3173faec87934d023e817fed16982f6
Brandon Man <brandonkingyiuman@gmail.com>
2019-02-13 17:02:54 -0800

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Brandon Man on 13/2/2019.
+//  Copyright © 2019 Brandon Man. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
