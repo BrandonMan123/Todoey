aba222c13a0f599e29934b98127485f0487e356e
Brandon Man <brandonkingyiuman@gmail.com>
2019-02-27 10:20:24 -0800

    Convert to MVC design pattern

---
+++ Todoey/DataModel/Item.swift
@@ -0,0 +1,15 @@
+//
+//  TodoListModel.swift
+//  Todoey
+//
+//  Created by Brandon Man on 23/2/2019.
+//  Copyright © 2019 Brandon Man. All rights reserved.
+//
+
+import Foundation
+
+class Item{
+    
+    var title: String = ""
+    var done: Bool = false
+}
